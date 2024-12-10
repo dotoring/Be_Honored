418e6ea83c5e67487d30e91a9cf615aa67d03891
YunSewon <ysw0823g@gmail.com>
2024-12-10 14:03:06 +0900

    소품모델들 추가

---
+++ Assets/02_Scripts/Test.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+	int as_fs_d;
+	string Tast_string;
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start()
+	{
+		as_fs_d = 0;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
