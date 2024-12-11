ea9c456e50b0cc7b2129e6ef2f7b8730389bba00
Tak <blueligh@naver.com>
2024-12-11 17:50:26 +0900

    Looby Env Setup , xritk origin setup, Player presetup {ing] , Equip setup {ing]

---
+++ Assets/02_Scripts/Player.cs
@@ -0,0 +1,55 @@
+using System;
+using Unity.VisualScripting;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public enum KindOfclass
+{
+	Warrior,
+	Mage,
+	Archer,
+	Priest
+}
+
+public struct Stat
+{
+	int str;
+	int dex;
+	int inteli;
+
+	public int Str { get => str; set => str = value; }
+	public int Dex { get => dex; set => dex = value; }
+	public int Inteli { get => inteli; set => inteli = value; }
+}
+
+public struct Armor
+{
+	public Equipment head;
+	public Equipment Body;
+	public Equipment Leg;
+	public Equipment Arm;
+
+}
+
+public class Player : MonoBehaviour
+{
+	[SerializeField] KindOfclass _myClass;
+	[SerializeField] Stat _stat;
+	[SerializeField] Stat _curStat;
+	[SerializeField] Armor _armor;
+
+	void ArmorChange()
+	{
+		SetStat(_armor.head);
+		SetStat(_armor.Body);
+		SetStat(_armor.Leg);
+		SetStat(_armor.Arm);
+	}
+
+	private void SetStat(Equipment equip)
+	{
+		_curStat.Str = _stat.Str + equip.str;
+		_curStat.Dex = _stat.Dex + equip.dex;
+		_curStat.Inteli = _stat.Inteli + equip.intelli;
+	}
+}
