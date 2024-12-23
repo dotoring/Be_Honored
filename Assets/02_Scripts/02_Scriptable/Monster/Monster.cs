c704b2cfc6c454da844c75785f81e579c5e337b3
SongJungHyun <kkrrasas8022@gmail.com>
2024-12-23 15:18:47 +0900

    Cerberus Prefab 수정

--- Assets/02_Scripts/02_Scriptable/Monster/Monster.cs
+++ Assets/02_Scripts/02_Scriptable/Monster/Monster.cs
@@ -45,13 +45,14 @@ public class Monster : MonoBehaviour
 		if (!PhotonNetwork.IsMasterClient)
 		{
 			behaviorAgent.enabled = false;
+			navMeshAgent.enabled = false;
 		}
-		//spawner = DungeonMgr.instance?.SetModule(moduleId).GetComponent<MonsterSpawner>();
-		//spawner.AddToList(this.gameObject);
+		spawner = DungeonMgr.instance?.SetModule(moduleId).GetComponent<MonsterSpawner>();
+		spawner.AddToList(this.gameObject);
 
 		dieEvent += () => spawner.RemoveFromList(this.gameObject);
 
-		//LoadData();
+		LoadData();
 	}
 
 	private void LoadData()
