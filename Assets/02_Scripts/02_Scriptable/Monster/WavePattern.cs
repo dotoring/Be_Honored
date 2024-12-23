d3bdd53d56e9189131696c83e7fae16cce6800a1
SongJungHyun <kkrrasas8022@gmail.com>
2024-12-23 17:47:10 +0900

    Mino 패턴 제작중

--- Assets/02_Scripts/02_Scriptable/Monster/WavePattern.cs
+++ Assets/02_Scripts/02_Scriptable/Monster/WavePattern.cs
@@ -3,15 +3,14 @@ using UnityEngine;
 public class WavePattern : MonoBehaviour
 {
 	public BossMonster bossMonster;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	//각도 0 90 180 270
+
+	//공 갯수 > 5개?
+
+	//패턴
+	//1. 공 갯수 중에 한군데 빵꾸
+	//2. 0 2 0 4 0 공 나오게
+	//3. 1 0 3 0 5 공 나오게
+
 }
