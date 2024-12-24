ffd50091fdef9c510b8aeef71425939f5d4db99d
SongJungHyun <kkrrasas8022@gmail.com>
2024-12-24 11:12:25 +0900

    WavePattern 제작중

--- Assets/02_Scripts/02_Scriptable/Monster/WavePattern.cs
+++ Assets/02_Scripts/02_Scriptable/Monster/WavePattern.cs
@@ -1,16 +1,72 @@
+using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
 
 public class WavePattern : MonoBehaviour
 {
 	public BossMonster bossMonster;
+	[SerializeField] private GameObject ironBall;
+	[SerializeField] private List<Transform> startPoints;
+	[SerializeField] private List<GameObject> currentBalls;
+	[SerializeField] private float speed = 3.0f;
 
+	[SerializeField] private float waitTime=1.0f;
+	[SerializeField] private float curTime=0.0f;
 	//각도 0 90 180 270
-
+	private void OnEnable()
+	{
+		curTime = 0;
+		transform.eulerAngles = Random.Range(0, 4) * 90.0f * Vector3.up;
+		PatternOne(Random.Range(0, startPoints.Count));
+	}
+	private void Update()
+	{
+		if (curTime < waitTime)
+			curTime += Time.deltaTime;
+		else
+		{
+			foreach (var ball in currentBalls)
+			{
+				ball.transform.position += ball.transform.forward * Time.deltaTime * speed;
+			}
+		}
+	}
+	private void OnDisable()
+	{
+		foreach(var ball in currentBalls)
+		{
+			Destroy(ball);
+		}
+		currentBalls.Clear();
+	}
 	//공 갯수 > 5개?
 
 	//패턴
 	//1. 공 갯수 중에 한군데 빵꾸
 	//2. 0 2 0 4 0 공 나오게
 	//3. 1 0 3 0 5 공 나오게
+
+	void PatternOne(int num)
+	{
+		for (int i = 0; i < startPoints.Count; i++)
+		{
+			if(i!=num)
+			{
+				GameObject ball = Instantiate(ironBall, startPoints[i]);
+				currentBalls.Add(ball);
+			}	
+		}
+	}
+	void PatternTwo(int num)
+	{
+		for (int i = 0; i < startPoints.Count; i++)
+		{
+			if (i%2==num)
+			{
+				GameObject ball = Instantiate(ironBall, startPoints[i]);
+				currentBalls.Add(ball);
+			}
+		}
+	}
 
 }
--- Assets/02_Scripts/03_BehaviorGraphs/WavePatternStart.cs
+++ Assets/02_Scripts/03_BehaviorGraphs/WavePatternStart.cs
@@ -5,16 +5,13 @@ using Action = Unity.Behavior.Action;
 using Unity.Properties;
 
 [Serializable, GeneratePropertyBag]
-[NodeDescription(name: "WavePatternStart", story: "[WaveObj] 를 활성화하고 [BossMonster] 를 넘겨줌", category: "Action", id: "a1037100e6555b31cbf547b0b10adcda")]
+[NodeDescription(name: "WavePatternStart", story: "[WaveObj] 를 활성화한다", category: "Action", id: "a1037100e6555b31cbf547b0b10adcda")]
 public partial class WavePatternStart : Action
 {
     [SerializeReference] public BlackboardVariable<GameObject> WaveObj;
-    [SerializeReference] public BlackboardVariable<BossMonster> BossMonster;
-
     protected override Status OnStart()
     {
 		WaveObj.Value.SetActive(true);
-		WaveObj.Value.GetComponent<WavePattern>().bossMonster = BossMonster.Value;
         return Status.Running;
     }
 
