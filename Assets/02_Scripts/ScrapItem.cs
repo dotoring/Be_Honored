76643adad5a0a41cb7ec51f8a0e4364e528e79a0
YunSewon <ysw0823g@gmail.com>
2024-12-24 09:42:54 +0900

    스크랩 잡기 권한 설정

--- Assets/02_Scripts/ScrapItem.cs
+++ Assets/02_Scripts/ScrapItem.cs
@@ -2,14 +2,18 @@ using System;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using Photon;
+using Photon.Pun;
 
 public class ScrapItem : MonoBehaviour
 {
 	XRGrabInteractable xRGrabInteractable;
+	PhotonView pv;
 
 	private void Awake()
 	{
 		xRGrabInteractable = GetComponent<XRGrabInteractable>();
+		pv = GetComponent<PhotonView>();
 	}
 
 	private void Start()
@@ -17,6 +21,15 @@ public class ScrapItem : MonoBehaviour
 
 		xRGrabInteractable.selectEntered.AddListener(grabed);
 
+		if (xRGrabInteractable != null)
+		{
+			xRGrabInteractable.selectEntered.AddListener((args) =>
+			{
+				//오브젝트의 PhotonView에서 Ownership Transfer를 Takeover로 설정하면 소유권(컨트롤러 포함)을 강제로 가져올 수 있도록 한다
+				//TransferOwnership(Player) -> 현재 PhotonView의 소유권을 Player로 바꾸는 함수
+				pv.TransferOwnership(PhotonNetwork.LocalPlayer);
+			});
+		}
 	}
 
 
