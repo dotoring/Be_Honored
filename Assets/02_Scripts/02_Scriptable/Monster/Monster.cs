8346d5a0bbf8573fd9ec0d47503d347b97a3f1b4
SongJungHyun <kkrrasas8022@gmail.com>
2025-01-02 16:50:49 +0900

    디버그 주석 삭제

--- Assets/02_Scripts/02_Scriptable/Monster/Monster.cs
+++ Assets/02_Scripts/02_Scriptable/Monster/Monster.cs
@@ -59,19 +59,19 @@ public class Monster : MonoBehaviourPunCallbacks
 	{
 
 		//디버그 할때 주석
-		//if (!PhotonNetwork.IsMasterClient)
-		//{
-		//	behaviorAgent.enabled = false;
-		//	navMeshAgent.enabled = false;
-		//	if(hpBar!=null)
-		//		hpBar.gameObject.SetActive(false);
-		//}
-		//spawner = DungeonMgr.instance?.SetModule(moduleId).GetComponent<MonsterSpawner>();
-		//spawner.AddToList(this.gameObject);
-		//dieEvent += () => spawner.RemoveFromList(this.gameObject);
-		//dieEvent += () => MainFactory.Inst.MonsterDrop(transform);
-		//if(typeOfMonster!=MonsterType.BOSS)
-		//	dieEvent += () => pv.RPC(nameof(DieRPC), RpcTarget.AllBuffered);
+		if (!PhotonNetwork.IsMasterClient)
+		{
+			behaviorAgent.enabled = false;
+			navMeshAgent.enabled = false;
+			if(hpBar!=null)
+				hpBar.gameObject.SetActive(false);
+		}
+		spawner = DungeonMgr.instance?.SetModule(moduleId).GetComponent<MonsterSpawner>();
+		spawner.AddToList(this.gameObject);
+		dieEvent += () => spawner.RemoveFromList(this.gameObject);
+		dieEvent += () => MainFactory.Inst.MonsterDrop(transform);
+		if(typeOfMonster!=MonsterType.BOSS)
+			dieEvent += () => pv.RPC(nameof(DieRPC), RpcTarget.AllBuffered);
 		LoadData();
 	}
 
