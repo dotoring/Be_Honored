d6c7a8d5e88f0ecd15b7a2ad9f3ed1c0e472c0f8
YunSewon <ysw0823g@gmail.com>
2025-01-06 16:51:10 +0900

    마스터 이탈 시 모든 몬스터 BT켜지는 문제 수정

--- Assets/02_Scripts/Dungeon/ModuleMgr.cs
+++ Assets/02_Scripts/Dungeon/ModuleMgr.cs
@@ -32,14 +32,17 @@ public class ModuleMgr : MonoBehaviour
 	{
 		Vector3 origin = transform.position + Vector3.up;
 		Vector3[] directions = { Vector3.forward, Vector3.back, Vector3.left, Vector3.right };
+		Vector3 size = new Vector3(2, 2, 2);
+		int layerMask = LayerMask.GetMask("Door");
 
 		foreach (var direction in directions)
 		{
-			if (Physics.Raycast(origin, direction, out RaycastHit hit, 5f))
+			if (Physics.BoxCast(origin, size, direction, out RaycastHit hit, Quaternion.identity, 5f, layerMask))
 			{
 				if (hit.collider.CompareTag("Door"))
 				{
 					hit.collider.transform.root.GetComponent<DoorCtrl>().OnDoorOpen += OnRoomOpen;
+					Debug.Log(hit.collider.transform.root.GetComponent<DoorCtrl>().isOpen);
 					this.isOpen = hit.collider.transform.root.GetComponent<DoorCtrl>().isOpen;
 				}
 			}
