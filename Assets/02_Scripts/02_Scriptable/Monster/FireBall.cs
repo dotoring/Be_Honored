1e2559519b702d174a12255342fe83521328efa4
SongJungHyun <kkrrasas8022@gmail.com>
2025-01-07 16:06:51 +0900

    Cerberus 수정 완료

--- Assets/02_Scripts/02_Scriptable/Monster/FireBall.cs
+++ Assets/02_Scripts/02_Scriptable/Monster/FireBall.cs
@@ -6,14 +6,15 @@ public class FireBall : MonoBehaviour
 	[SerializeField]private BossMonster boss;
 	[SerializeField]private GameObject target;
 	[SerializeField]private GameObject floor;
-	[SerializeField] private Transform startPoint;
+	[SerializeField] private Vector3 startPoint;
 	[SerializeField]private float speed;
 
 	public void InitBall(BossMonster bossMonster,GameObject target,Transform startPoint)
 	{
 		boss = bossMonster;
 		this.target = target;
-		this.startPoint=startPoint;
+		this.startPoint=startPoint.position;
+		speed = Vector3.Distance(target.transform.position, this.startPoint)/2.5f;
 	}
 
 	private void OnDrawGizmos()
@@ -24,7 +25,7 @@ public class FireBall : MonoBehaviour
 
 	private void Update()
 	{
-		transform.position += speed * Time.deltaTime * (target.transform.position - startPoint.position).normalized;
+		transform.position += speed * Time.deltaTime * (target.transform.position - startPoint).normalized;
 	}
 
 	private void OnTriggerEnter(Collider other)
