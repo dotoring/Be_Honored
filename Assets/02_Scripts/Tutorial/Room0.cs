2afe3d4b32e7703803b27990a6b7d7d326da077c
Tak <blueligh@naver.com>
2025-01-21 17:48:56 +0900

    tutorial working for Korean ,armory , equip , and Voice

--- Assets/02_Scripts/Tutorial/Room0.cs
+++ Assets/02_Scripts/Tutorial/Room0.cs
@@ -7,6 +7,8 @@ using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
 public class Room0 : MonoBehaviour
 {
+    [SerializeField] TutoManager _manager;
+    [SerializeField] AudioSource audioSource;
     [SerializeField] InputActionProperty move;
     [SerializeField] InputActionProperty turn;
     [SerializeField] private XRSimpleInteractable pokeButton;
@@ -14,41 +16,55 @@ public class Room0 : MonoBehaviour
     bool NextStep;
     [SerializeField] GameObject NextRoomSeq;
     [SerializeField] private GameObject nextRoomBtnText;
-
+    
     private void Start()
     {
-        text.text = "Welcome to the New World";
+        //text.text = "Welcome to the New World";
+        text.text = "새로운 세계에 오신것을 환영합니다.";
+        
+        
+        
 
-    nextRoomBtnText.SetActive(false);
+        nextRoomBtnText.SetActive(false);
         StartCoroutine(seq());
     }
 
     IEnumerator seq()
     {
+        yield return new WaitForSeconds(1.5f);
+        audioSource.clip = _manager.audioClips[0];
+        audioSource.Play();
         pokeButton.enabled = false;
         yield return new WaitForSeconds(3f);
-        text.text = "This is Tutorial for BeHorned";
+        text.text = "이곳은 튜토리얼 공간입니다.";
         yield return new WaitForSeconds(3f);
-        text.text = "At First, You can Move by Left Hand Thumbstick";
+        audioSource.clip = _manager.audioClips[1];
+        audioSource.Play();
+        text.text = "우선 왼손 엄지로 조이스틱을 움직여서 케릭터를 이동시켜 보세요";
         yield return new WaitForSeconds(3f);
-        text.text = "Move Your charicter";
+        //text.text = "Move Your charicter";
 
         move.action.performed += ActionOnPerformed;
         yield return new WaitUntil(() => NextStep);
         move.action.performed -= ActionOnPerformed;
         NextStep = false;
-
-
-        text.text = "Good Job, then Trun your charicter";
-        text.text = "You can Trun by right Hand Thumbstick";
+        
+        
+        audioSource.clip = _manager.audioClips[2];
+        audioSource.Play();
+        text.text = "잘하셨습니다. 오른손의 조이스틱을 움직여 케릭터를 회전시켜 보세요";
+        //text.text = "You can Trun by right Hand Thumbstick";
         turn.action.performed += ActionOnPerformed;
         yield return new WaitUntil(() => NextStep);
         turn.action.performed -= ActionOnPerformed;
         NextStep = false;
         pokeButton.enabled = true;
-    nextRoomBtnText.SetActive(true);
-        text.text = "Great, Step in This room is over";
-        text.text = "You can Go next room by click button";
+        nextRoomBtnText.SetActive(true);
+        
+        audioSource.clip = _manager.audioClips[3];
+        audioSource.Play();
+        text.text = "수고하셨습니ㅏㄷ. 다음방 가는 버튼을 눌러 다음방 문을 열어보세요";
+        //text.text = "You can Go next room by click button";
         pokeButton.selectEntered.AddListener(_ => { NextRoomSeq.SetActive(true); });
 
         yield return new WaitForSeconds(3f);
--- Assets/02_Scripts/Tutorial/Room3.cs
+++ Assets/02_Scripts/Tutorial/Room3.cs
@@ -5,25 +5,31 @@ using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
 public class Room3 : MonoBehaviour
 {
+    [SerializeField] TutoManager _manager;
+    [SerializeField] AudioSource audioSource;
     [SerializeField] private XRSimpleInteractable pokeButton;
     [SerializeField] private TMP_Text text;
     bool NextStep;
     [SerializeField] GameObject NextRoomSeq;
-    //[SerializeField] private GameObject nextRoomBtnText;
+    [SerializeField] private GameObject canvas;
     
     private void Start()
     {
-        text.text = "This room is Last Room";
+        text.text = "마지막 방입니다.";
+        
         //nextRoomBtnText.SetActive(false);
         StartCoroutine(seq());
     }
 
     IEnumerator seq()
     {
+        yield return new WaitUntil( ()  => canvas.activeInHierarchy);
+        audioSource.clip = _manager.audioClips[9];
+        audioSource.Play();
         //pokeButton.enabled = false;
         yield return null;
         yield return new WaitForSeconds(3);
-        text.text = "Move to Potal For Adventure";
+        text.text = "모험을 위해 포탈로 들어가 새로운 모험을 시작하세요";
     }
 
     private void ActionOnPerformed()
