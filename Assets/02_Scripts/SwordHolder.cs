2da5fde6bb27fba4603e943b721bda23ec544391
YunSewon <ysw0823g@gmail.com>
2025-01-24 16:02:09 +0900

    가방센서, 칼집센서 인식 성능개선

---
+++ Assets/02_Scripts/SwordHolder.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+
+public class SwordHolder : MonoBehaviour
+{
+    //칼이 홀더에 있을 때 홀더를 잡으면 칼을 쥐어주기
+    //칼이 홀더에 없을 때 작동 안하기
+    //그냥 칼을 잡아서 가져갈때 홀더에 칼 있음 끄기
+
+    public bool isEmpty;
+    [SerializeField] private XRGrabInteractable swordGI;
+    [SerializeField] XRGrabInteractable xrGrab;
+
+    private void Start()
+    {
+	    xrGrab.selectEntered.AddListener(OnSelectEntered);
+    }
+
+    private void OnDestroy()
+    {
+	    xrGrab.selectEntered.RemoveListener(OnSelectEntered);
+    }
+
+    private void OnSelectEntered(SelectEnterEventArgs args)
+    {
+	    if (isEmpty) return;
+	    //잡기 이벤트만 발생시키고 잡기 해제
+	    xrGrab.interactionManager.CancelInteractableSelection(args.interactableObject);
+	    swordGI.interactionManager.SelectEnter(args.interactorObject, swordGI);
+    }
+}
