a7994d7aee92ea9ad51de2560889824097bad92c
SongJungHyun <kkrrasas8022@gmail.com>
2025-02-04 10:06:02 +0900

    fireball fixing

--- Assets/Hovl Studio/HSFiles/Scripts/HS_CallBackParent.cs
+++ Assets/Hovl Studio/HSFiles/Scripts/HS_CallBackParent.cs
@@ -6,15 +6,15 @@ public class HS_CallBackParent : MonoBehaviour
 {
     [SerializeField]protected Transform parentObject;
 
-    protected virtual void OnParticleSystemStopped()
-    {
-        if (parentObject != null)
-        {
-            transform.parent = parentObject;
-            transform.localPosition = Vector3.zero;
-            transform.localEulerAngles = Vector3.zero;
-        }
-        else
-            Destroy(gameObject);
-    }
+    //protected virtual void OnParticleSystemStopped()
+    //{
+    //    if (parentObject != null)
+    //    {
+    //        transform.parent = parentObject;
+    //        transform.localPosition = Vector3.zero;
+    //        transform.localEulerAngles = Vector3.zero;
+    //    }
+    //    else
+    //        Destroy(gameObject);
+    //}
 }
