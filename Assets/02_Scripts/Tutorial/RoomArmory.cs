eec38552062b596730d1da82f9423f04b1426df5
Tak <blueligh@naver.com>
2025-02-05 11:27:40 +0900

    tutorial fix for class

--- Assets/02_Scripts/Tutorial/RoomArmory.cs
+++ Assets/02_Scripts/Tutorial/RoomArmory.cs
@@ -5,70 +5,70 @@ using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
 public class RoomArmory : MonoBehaviour
 {
-    [SerializeField] TutoManager _manager;  // 사운드클립있음
-    [SerializeField] AudioSource audioSource; // xrorigin용
-    [SerializeField] private XRSimpleInteractable pokeButton;  // 다음방 가는 버튼
-    [SerializeField] private TMP_Text text; // 텍스트 설명
-    bool NextStep;
-    [SerializeField] GameObject NextRoomSeq;
-    [SerializeField] private GameObject nextRoomBtnText;
-    [SerializeField] private GameObject canvas;  // activation check
-    [SerializeField] private XRBaseInteractable grab;
-    [SerializeField] private Tutoequip tutoequip;
-    private void Start()
-    {
-        text.text = "Welcome to the New World";
+	[SerializeField] TutoManager _manager;  // 사운드클립있음
+	[SerializeField] AudioSource audioSource; // xrorigin용
+	[SerializeField] private XRSimpleInteractable pokeButton;  // 다음방 가는 버튼
+	[SerializeField] private TMP_Text text; // 텍스트 설명
+	bool NextStep;
+	[SerializeField] GameObject NextRoomSeq;
+	[SerializeField] private GameObject nextRoomBtnText;
+	[SerializeField] private GameObject canvas;  // activation check
+	[SerializeField] private XRBaseInteractable grab;
+	[SerializeField] private Tutoequip tutoequip;
+	private void Start()
+	{
+		text.text = "Welcome to the New World";
 
 
-        nextRoomBtnText.SetActive(false);
-        StartCoroutine(seq());
-    }
+		nextRoomBtnText.SetActive(false);
+		StartCoroutine(seq());
+	}
 
-    IEnumerator seq()
-    {
-        yield return new WaitUntil(() => canvas.activeInHierarchy);
-        audioSource.clip = _manager.audioClips[10];
-        audioSource.Play();
-        text.text = "이방은 장비에 관한 방입니다. 왼쪽에 바지가 있습니다. \n장비는 적을 처치시 획득할수있습니다. 장비를 들어보세요";
-        pokeButton.enabled = false;
-        yield return new WaitForSeconds(3f);  // 시작 루틴
-        
-        //집어 올리고
-        grab.selectEntered.AddListener( _ => ActionOnPerformed());
-        
-        yield return new WaitUntil(() => NextStep);
-        grab.selectEntered.RemoveAllListeners();
-        NextStep = false;
-        
-        
-        //장비를 장착하고 
-        audioSource.clip = _manager.audioClips[11];
-        audioSource.Play();
-        text.text = "장비를 오른쪽 큐브모양의 슬롯에 넣어보세요 \n이곳은 튜토리얼이라 던전에서 장비를 입을수 있지만 \n실제 던전에서는 마을에 가셔야 합니다.";
-        tutoequip.equip += ActionOnPerformed;
-        yield return new WaitUntil(() => NextStep);
-        tutoequip.equip -= ActionOnPerformed;
-        NextStep = false;
-        yield return new WaitForSeconds(1f);
-        audioSource.clip = _manager.audioClips[12];
-        audioSource.Play();
-        text.text = "장비는 마을의 무기점에 가서 장비를 적합한 슬롯에 넣는것으로 장비할 수 있고, 그에따라 능력이 향상됩니다.";
-        
-        
-        // 다음방 가는 루틴
-        pokeButton.enabled = true;
-        nextRoomBtnText.SetActive(true);
-        text.text = "수고하셨습니다. 다음방 가는 버튼을 눌러 다음방 문을 열어보세요";
-        pokeButton.selectEntered.AddListener(_ => { NextRoomSeq.SetActive(true); }); 
-        audioSource.clip = _manager.audioClips[3];
-        audioSource.Play();
-        yield return new WaitForSeconds(3f);
-        
-    }
-    
-    private void ActionOnPerformed()
-    {
-        Debug.Log("check the action");
-        NextStep = true;
-    }
+	IEnumerator seq()
+	{
+		yield return new WaitUntil(() => canvas.activeInHierarchy);
+		audioSource.clip = _manager.audioClips[10];
+		audioSource.Play();
+		text.text = "이방은 장비에 관한 방입니다. 왼쪽에 바지가 있습니다. \n장비는 적을 처치시 획득할수있습니다. 장비를 들어보세요";
+		pokeButton.enabled = false;
+		//yield return new WaitForSeconds(3f);  // 시작 루틴
+
+		//집어 올리고
+		grab.selectEntered.AddListener(_ => ActionOnPerformed());
+
+		yield return new WaitUntil(() => NextStep);
+		grab.selectEntered.RemoveAllListeners();
+		NextStep = false;
+
+
+		//장비를 장착하고
+		audioSource.clip = _manager.audioClips[11];
+		audioSource.Play();
+		text.text = "장비를 오른쪽 큐브모양의 슬롯에 넣어보세요 \n이곳은 튜토리얼이라 던전에서 장비를 입을수 있지만 \n실제 던전에서는 마을에 가셔야 합니다.";
+		tutoequip.equip += ActionOnPerformed;
+		yield return new WaitUntil(() => NextStep);
+		tutoequip.equip -= ActionOnPerformed;
+		NextStep = false;
+		//yield return new WaitForSeconds(1f);
+		audioSource.clip = _manager.audioClips[12];
+		audioSource.Play();
+		text.text = "장비는 마을의 무기점에 가서 장비를 적합한 슬롯에 넣는것으로 장비할 수 있고, 그에따라 능력이 향상됩니다.";
+
+
+		// 다음방 가는 루틴
+		pokeButton.enabled = true;
+		nextRoomBtnText.SetActive(true);
+		text.text = "수고하셨습니다. 다음방 가는 버튼을 눌러 다음방 문을 열어보세요";
+		pokeButton.selectEntered.AddListener(_ => { NextRoomSeq.SetActive(true); });
+		audioSource.clip = _manager.audioClips[3];
+		audioSource.Play();
+		// yield return new WaitForSeconds(3f);
+
+	}
+
+	private void ActionOnPerformed()
+	{
+		Debug.Log("check the action");
+		NextStep = true;
+	}
 }
--- Assets/02_Scripts/Tutorial/TutoSandBag.cs
+++ Assets/02_Scripts/Tutorial/TutoSandBag.cs
@@ -5,31 +5,32 @@ using UnityEngine.UI;
 
 public class TutoSandBag : MonoBehaviour
 {
-    [SerializeField] private Image hpImg;
-    public float hp = 1;
-    public Action die;
-    [SerializeField] private GameObject tutoEq;
-    private bool isWorking = false;
-    private void Start()
-    {
-        hpImg.rectTransform.sizeDelta = new Vector2(1, hpImg.rectTransform.sizeDelta.y);
-    }
+	[SerializeField] private Image hpImg;
+	public float hp = 1;
+	public Action die;
+	[SerializeField] private GameObject tutoEq;
+	private bool isWorking = false;
+	private void Start()
+	{
+		hpImg.rectTransform.sizeDelta = new Vector2(1, hpImg.rectTransform.sizeDelta.y);
+	}
 
-    public void WorkingOn()
-    {
-        isWorking = true;
-    }
+	public void WorkingOn()
+	{
+		isWorking = true;
+	}
 
-    public void Damaged()
-    {
-        if (!isWorking) return;
-        hp -= 0.5f;
-        hpImg.rectTransform.sizeDelta = new Vector2(hp, hpImg.rectTransform.sizeDelta.y);
-        if (hp <= 0)
-        {
-            Destroy(gameObject,1f);
-            die.Invoke();
-            //Instantiate(tutoEq, transform.position, Quaternion.identity);
-        }
-    }
+	public void Damaged()
+	{
+		if (!isWorking) return;
+		hp -= 0.5f;
+		hpImg.rectTransform.sizeDelta = new Vector2(hp, hpImg.rectTransform.sizeDelta.y);
+		if (hp <= 0)
+		{
+			Destroy(gameObject, 1f);
+			isWorking = false;
+			die.Invoke();
+			//Instantiate(tutoEq, transform.position, Quaternion.identity);
+		}
+	}
 }
